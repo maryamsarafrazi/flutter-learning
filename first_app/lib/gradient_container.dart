b8879b0545af3c32d13553a2612968db09e4df33
Hadi Samadzad <hadisamadzad@Hadis-MBP.broadband>
2025-01-27 01:19:52 +0000

    Make grandient container widget resuable

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -5,16 +5,16 @@ const startAlignment = Alignment.topCenter;
 const endAlignment = Alignment.bottomCenter;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.colors, {super.key});
+
+  final List<Color> colors;
+
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
+          colors: colors,
           begin: startAlignment,
           end: endAlignment,
         ),
