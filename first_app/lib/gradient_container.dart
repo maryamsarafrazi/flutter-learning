ded8ddda85087a84a890244ec38a20d7b426e212
Hadi Samadzad <hadisamadzad@Hadis-MBP.broadband>
2025-01-27 00:07:25 +0000

    Add init files of first app project

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,24 @@
+import 'package:first_app/styled_text.dart';
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
---
+++ first_app/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:first_app/gradient_container.dart';
+import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
