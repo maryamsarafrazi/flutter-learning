03392ab0b9b86df38568b486e3069a842eed2a5a
Hadi Samadzad <h.samadzad@gmail.com>
2025-01-29 22:28:02 +0000

    Init images in roll dice function

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
 
 const startAlignment = Alignment.topCenter;
 const endAlignment = Alignment.bottomCenter;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.colors, {super.key});
+  GradientContainer(this.colors, {super.key});
 
   final List<Color> colors;
+
+  var activeDiceImage = 'assets/images/dice-2.png';
+
+  void rollDice() {
+    activeDiceImage = 'assets/images/dice-2.png';
+    print('Changing the dice ...');
+  }
 
   @override
   Widget build(context) {
@@ -20,7 +26,25 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: StyledText("My Hello Worlds!"),
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Image.asset(
+            activeDiceImage,
+            width: 200,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+              textStyle: TextStyle(
+                fontSize: 28,
+              ),
+            ),
+            child: Text("Roll"),
+          ),
+        ]),
       ),
     );
   }
